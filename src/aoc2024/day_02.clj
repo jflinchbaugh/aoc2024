724f20118342818b1f5bd62457a0702bcf68e917
John Flinchbaugh <john@hjsoft.com>
2024-12-03 09:08:45 -0500

    format

--- src/aoc2024/day_02.clj
+++ src/aoc2024/day_02.clj
@@ -10,64 +10,53 @@
 
 (defn safe? [reports]
   (->>
-    reports
-    (partition 2 1)
-    (map (fn [[a b]] (- a b)))
-    ((fn [rs]
-       (or
-         (small-steps-up? rs)
-         (small-steps-down? rs)))))
-
-  )
+   reports
+   (partition 2 1)
+   (map (fn [[a b]] (- a b)))
+   ((fn [rs]
+      (or
+       (small-steps-up? rs)
+       (small-steps-down? rs))))))
 
 (defn parse-line [s]
   (map parse-long (str/split s #" ")))
 
 (defn part-1 [input]
   (->>
-    input
-    str->lines
-    (map parse-line)
-    (filter safe?)
-    count
-    )
-  )
+   input
+   str->lines
+   (map parse-line)
+   (filter safe?)
+   count))
 
 (comment
   (->> "input/day_02.txt"
-    slurp
-    part-1)
+       slurp
+       part-1)
   ;; => 479
-
   )
-
 (defn dampen [reports]
   (cons reports
-    (for [to-remove (range (count reports))]
-      (keep-indexed (fn [i o] (if (not= i to-remove) o)) reports)))
-  )
+        (for [to-remove (range (count reports))]
+          (keep-indexed (fn [i o] (if (not= i to-remove) o)) reports))))
 
 (defn part-2 [input]
   (->>
-    input
-    str->lines
-    (map parse-line)
-    (map
-      (fn [reports]
-        (->>
-          reports
-          dampen
-          (some safe?)
-          )))
-    (keep identity)
-    count
-    )
-  )
+   input
+   str->lines
+   (map parse-line)
+   (map
+    (fn [reports]
+      (->>
+       reports
+       dampen
+       (some safe?))))
+   (keep identity)
+   count))
 
 (comment
   (->> "input/day_02.txt"
-    slurp
-    part-2)
+       slurp
+       part-2)
   ;; => 531
-
   )
