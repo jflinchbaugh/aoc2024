3d5a437c6bf41be2f2a2b09983054fa531fe497c
John Flinchbaugh <john@hjsoft.com>
2024-12-03 23:00:55 -0500

    day 3 part 2

--- src/aoc2024/day_03.clj
+++ src/aoc2024/day_03.clj
@@ -7,6 +7,35 @@
 (defn part-1 [input]
   (->>
     input
+    str->lines
+    (str/join)
+    (re-seq mul-regex)
+    (map (fn [[_ a b]] (* (parse-long a) (parse-long b))))
+    (reduce +)
+    ))
+
+(comment
+  (->> "input/day_03.txt"
+    slurp
+    part-1)
+  ;; => 159833790
+  )
+
+(def dont-do-regex #"don't\(\).*?do\(\)")
+(def dont-regex #"don't\(\).*")
+
+(defn remove-dont-sections [s]
+  (-> s
+    (str/replace dont-do-regex "")
+    (str/replace dont-regex "")
+    ))
+
+(defn part-2 [input]
+  (->>
+    input
+    str->lines
+    (str/join)
+    (remove-dont-sections)
     (re-seq mul-regex)
     (map (fn [[_ a b]] (* (parse-long a) (parse-long b))))
     (reduce +)
@@ -15,6 +44,7 @@
 (comment
   (->> "input/day_03.txt"
        slurp
-       part-1)
-  ;; => 159833790
+       part-2)
+  ;; => 89349241
+
   )
--- test/aoc2024/day_03_test.clj
+++ test/aoc2024/day_03_test.clj
@@ -8,3 +8,12 @@ xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
 
 (t/deftest test-part-1
   (t/is (= 161 (part-1 input-1))))
+
+(def input-2 "
+xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+don't()anything elsemul(2,2)
+mul(3,5)
+")
+
+(t/deftest test-part-2
+  (t/is (= 48 (part-2 input-2))))
