3fab82b997d6fb8c382c337c4c66f9b2c4c4b5c0
John Flinchbaugh <john@hjsoft.com>
2024-12-05 22:51:05 -0500

    day 4 part 2

--- src/aoc2024/day_04.clj
+++ src/aoc2024/day_04.clj
@@ -11,62 +11,106 @@
   (let [chars (->> input
                    str->lines
                    (mapv vec))]
-      (for [coord (grid chars)]
-        (cons (get-in chars coord) coord))
-     ))
+    (for [coord (grid chars)]
+      (cons (get-in chars coord) coord))))
 
 (defn make-segment [[dx dy] [x y]]
   (for [step (range 4)]
     (let [nx (+ (* dx step) x)
           ny (+ (* dy step) y)]
       (when (and (>= nx 0) (>= ny 0))
-              [nx ny]))))
+        [nx ny]))))
 
 (defn lookup [letters [x y]]
   (->>
-    letters
-    (filter
-      (fn [[_ lx ly]]
-        (= [lx ly] [x y])))
-    first
-    first
-    ))
+   letters
+   (filter
+    (fn [[_ lx ly]]
+      (= [lx ly] [x y])))
+   first
+   first))
+
+(defn make-segments
+  [starts]
+  (->>
+   (mapcat
+    (fn [[dx dy]]
+      (map (partial make-segment [dx dy]) starts))
+    (for [x (range -1 2)
+          y (range -1 2)
+          :when (not= 0 x y)]
+      [x y]))
+   (remove (partial some nil?))))
+
+(defn make-words
+  [letters segments]
+  (map
+   (fn [segment]
+     (map
+      (fn [pos] (lookup letters pos))
+      segment))
+   segments))
 
 (defn part-1 [input]
   (let [letters (->>
-                  input
-                  (parse-word-search))
+                 input
+                 (parse-word-search))
         starts (->>
-                 letters
-                 (filter (comp #{\X} first))
-                 (map rest))
-        segments (->>
-                   (mapcat
-                     (fn [[dx dy]]
-                       (map (partial make-segment [dx dy]) starts))
-                     (for [x (range -1 2)
-                           y (range -1 2)
-                           :when (not= 0 x y)]
-                       [x y]))
-                   (remove (partial some nil?) ))
-        words (map
-          (fn [segment]
-            (map
-              (fn [pos] (lookup letters pos))
-              segment))
-          segments)]
+                letters
+                (filter (comp #{\X} first))
+                (map rest))
+        segments (make-segments starts)
+        words (make-words letters segments)]
 
     (->>
-      words
-      (filter
-        (fn [w] (= "XMAS" (str/join w))))
-      count)))
+     words
+     (filter
+      (fn [w] (= "XMAS" (str/join w))))
+     count)))
 
 (comment
   (->> "input/day_04.txt"
        slurp
        part-1)
   ;; => 2543
+  )
 
+(defn make-x-segment [[x y]]
+  (->>
+   [[-1 -1] [1 -1] [1 1] [-1 1]]
+   (map
+    (fn [[dx dy]]
+      (let [nx (+ x dx)
+            ny (+ y dy)]
+        (when (and (>= nx 0) (>= ny 0))
+          [nx ny]))))))
+
+(defn make-x-segments [starts]
+  (->>
+   starts
+   (map make-x-segment)
+   (remove (partial some nil?))))
+
+(defn part-2 [input]
+  (let [letters (->>
+                 input
+                 (parse-word-search))
+        starts (->>
+                letters
+                (filter (comp #{\A} first))
+                (map rest))
+        segments (make-x-segments starts)
+        words (make-words letters segments)]
+    (->>
+     words
+     (filter
+      (fn [w] (#{"MMSS" "SMMS" "SSMM" "MSSM"} (str/join w))))
+     count)))
+
+(comment
+  (->> "input/day_04.txt"
+       slurp
+       part-2)
+  ;; => 1930
 
   )
--- test/aoc2024/day_04_test.clj
+++ test/aoc2024/day_04_test.clj
@@ -2,6 +2,13 @@
   (:require  [aoc2024.day-04 :refer :all]
              [clojure.test :as t]))
 
+(t/deftest test-lookup
+  (t/is (= \M (lookup [[\M 0 0]] [0 0]))))
+
+(t/deftest test-make-segment
+  (t/is (= [[0 0] [1 1] [2 2] [3 3]] (make-segment [1 1] [0 0])))
+  (t/is (= [[0 0] [1 0] [2 0] [3 0]] (make-segment [1 0] [0 0])))
+  (t/is (= [[0 0] nil nil nil] (make-segment [-1 -1] [0 0]))))
 
 (def input-1 "
 MMMSXXMASM
@@ -16,9 +23,8 @@ MAMMMXMMMM
 MXMXAXMASX
 ")
 
-
-(t/deftest test-lookup
-  (t/is (= \M (lookup [[\M 0 0]] [0 0]))))
-
 (t/deftest test-part-1
   (t/is (= 18 (part-1 input-1))))
+
+(t/deftest test-part-2
+  (t/is (= 9 (part-2 input-1))))
